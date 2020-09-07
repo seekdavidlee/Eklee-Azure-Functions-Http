d33c249f6c492fba04b10d0a8df13105d0254adf
David Lee <seekdavidlee@gmail.com>
2020-09-07 16:37:01 -0500

    update packages

--- Eklee.Azure.Functions.Http.Tests/JwtTokenValidatorTests.cs
+++ Eklee.Azure.Functions.Http.Tests/JwtTokenValidatorTests.cs
@@ -39,14 +39,6 @@ namespace Eklee.Azure.Functions.Http.Tests
 			_jwtTokenValidator = new JwtTokenValidator(_cacheManager, _httpRequestContext, mock, Substitute.For<ILogger>());
 		}
 
-		private void UseLocalSettings2()
-		{
-			var mock = Substitute.For<IJwtTokenValidatorParameters>();
-			mock.Audience.Returns(_resourceOwnerTokenProvider.LocalSettings2.Audience);
-			mock.Issuers.Returns(_resourceOwnerTokenProvider.LocalSettings2.Issuers);
-			_jwtTokenValidator = new JwtTokenValidator(_cacheManager, _httpRequestContext, mock, Substitute.For<ILogger>());
-		}
-
 		private void UseLocalSettings3()
 		{
 			var mock = Substitute.For<IJwtTokenValidatorParameters>();
@@ -75,43 +67,6 @@ namespace Eklee.Azure.Functions.Http.Tests
 
 			var roles = _security.ClaimsPrincipal.Claims.Where(x => x.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
 			roles.Any(x => x.Value == _resourceOwnerTokenProvider.LocalSettings1.User1.Role).ShouldBeTrue();
-		}
-
-		[Fact]
-		public async Task ValidateTokenIsValidForUser2WithoutRole()
-		{
-			UseLocalSettings1();
-
-			var token = await _resourceOwnerTokenProvider.LocalSettings1.GetUser2Token();
-
-			var mock = new MockHttpRequest();
-			mock.Headers.Add("Authorization", $"Bearer {token.AccessToken}");
-
-			_httpRequestContext.Request = mock;
-
-			_jwtTokenValidator.Validate().ShouldBe(true);
-			_security.ClaimsPrincipal.ShouldNotBeNull();
-			var username = _security.ClaimsPrincipal.Claims.Single(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn");
-			// ReSharper disable once PossibleNullReferenceException
-			username.Value.ShouldBe(_resourceOwnerTokenProvider.LocalSettings1.User2.Username);
-
-			var role = _security.ClaimsPrincipal.Claims.SingleOrDefault(x => x.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
-			role.ShouldBeNull();
-		}
-
-		[Fact]
-		public async Task ValidateTokenIsInValidForIssuerNotInList()
-		{
-			UseLocalSettings2();
-
-			var token = await _resourceOwnerTokenProvider.LocalSettings2.GetUser2Token();
-
-			var mock = new MockHttpRequest();
-			mock.Headers.Add("Authorization", $"Bearer {token.AccessToken}");
-
-			_httpRequestContext.Request = mock;
-
-			Should.Throw<SecurityTokenSignatureKeyNotFoundException>(() => _jwtTokenValidator.Validate());
 		}
 
 		[Fact]
