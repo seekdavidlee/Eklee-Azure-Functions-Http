7672c74b1b72bd0f68553f96d1f48854ad88ea26
David Lee <seekdavidlee@gmail.com>
2020-11-26 09:08:52 -0600

    introduce debug flag for creating nuget package and fix create nuget err

--- PrepNugetPack.ps1
+++ PrepNugetPack.ps1
@@ -9,6 +9,5 @@ if (![System.IO.File]::Exists($filePath)){
     Get-ChildItem -Path "$Path\$app\bin\Release\netstandard2.1\bin"
 }
 
-Move-Item -Path $filePath -Destination "$Path\$app\bin\Release\netstandard2.1\$app.dll"
 Remove-Item -Path "$Path\$app\bin\Release\netstandard2.1\bin" -Recurse
 Copy-Item "$Path\LICENSE" "$Path\LICENSE.txt"
